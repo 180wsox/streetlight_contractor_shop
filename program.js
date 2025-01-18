7385cb5eb004f2b497e43d783ff0301985456700
468SiliconWP445 <hotlap8878@gmail.com>
2025-01-18 13:53:55 +0000

    Update program.js

--- program.js
+++ program.js
@@ -1,1 +1,14 @@
+class Streetlight {
+  
+  constructor(brand, model, type, wattage, lifespan, ballast) {
+    this.brand = brand;
+    this.model = model
+    this.type = type;
+    this.wattage = wattage;
+    this.lifespan = lifespan; //in years for the moment
+    this.ballast = ballast;
+  }
+  
+}
 
+let MA90 = new Streetlight("Philips", "MA90", "SOX", 90, 5, "BSX90L");

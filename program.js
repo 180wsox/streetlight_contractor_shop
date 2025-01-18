ca456b6802fd455fe29eccf6ff18f7d6c0fb93e3
468SiliconWP445 <hotlap8878@gmail.com>
2025-01-18 14:30:43 +0000

    Update program.js

--- program.js
+++ program.js
@@ -1,1 +1,13 @@
 console.log("JavaScript initialised successfully.");
+function downloadPage() {
+  const htmlContent = document.documentElement.outerHTML; //Retrieve HTML content (full)
+  const blob = new Blob([htmlContent], { type: "text/html" }); //binary data (blob)
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "placeholder.html";
+  link.click();
+}
+
+//Examples below
+//Add to page: <button class="download-page">Download Webpage</button> 
+//Add to page: document.querySelector(".download-page").addEventListener("click", downloadPage);
